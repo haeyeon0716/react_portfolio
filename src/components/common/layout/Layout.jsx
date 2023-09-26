0160a289bb6033eb1609738d1a8d58cf8253aaff
yhy7805 <yhy7805@gmail.com>
2023-09-26 14:33:17 +0900

    props 로 전달되는 값들을 비구조화할당으로 개선

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,15 +1,15 @@
 import styles from './Layout.module.scss';
 import clsx from 'clsx'
 
-export default function Layout(props) {
+export default function Layout({title, children}) {
+    console.log(props);
   return (
     <section className={clsx(styles.layout)}>
         <figure></figure>
 
         <div className={clsx(styles.content)}>
-            <h1>Sub Page Title</h1>
-            {/* 해당 컴포넌트로 특정 컨텐츠를 감싸면 감싸져 있는 내용들이 props.children으로 전달됨 */}
-            {props.children}
+            <h1>{title}</h1>
+            {children}
         </div>
     </section>
   );
