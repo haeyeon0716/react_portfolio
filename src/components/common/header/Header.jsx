0b08640b2891460cef27615830867b494bc7a58f
yhy7805 <yhy7805@gmail.com>
2023-09-26 15:15:43 +0900

    모든 컴포넌트 연결

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,17 +1,30 @@
 import styles from './Header.module.scss';
 import clsx from 'clsx';
+import {Link, NavLink} from 'react-router-dom';
 
 export default function Header() {
   return (
   <header className={clsx(styles.header, 'on')}>
-    <h1>LOGO</h1>
+    <h1>
+      <Link to='/'>LOGO</Link>
+    </h1>
 
     <ul>
-        <li>Department</li>
-        <li>Gallery</li>
-        <li>Youtube</li>
-        <li>Members</li>
-        <li>Contact</li>
+        <li>
+          <NavLink to='/department'>Department</NavLink>
+        </li>
+        <li>
+        <NavLink to='/Gallery'>Gallery</NavLink>
+        </li>
+        <li>
+        <NavLink to='/Youtube'>Youtube</NavLink>
+        </li>
+        <li>
+        <NavLink to='/Members'>Members</NavLink>
+        </li>
+        <li>
+        <NavLink to='/Contact'>Contact</NavLink>
+        </li>
     </ul>
 
   </header>
